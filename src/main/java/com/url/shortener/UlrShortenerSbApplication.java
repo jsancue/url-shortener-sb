d2d29957e656ce20ad96f3f54db11491586b6e4c
Jesús Sánchez Cuevas <jsancue2103@gmail.com>
2025-05-01 16:43:54 +0200

    URL SHORTENER

---
+++ src/main/java/com/url/shortener/UlrShortenerSbApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlrShortenerSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlrShortenerSbApplication.class, args);
+	}
+
+}
